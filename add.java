a3c709fa0713f81f56e91ff8493d5d7c900d7cfe
vigneshVijayakumar27 <vicky.v20040327@gmail.com>
2024-07-22 12:18:32 +0530

    Add files via upload

---
+++ add.java
@@ -0,0 +1,29 @@
+class ading
+{
+   public void  formateing()
+	{
+	   byte x=24;
+	   short y=123;
+	   int z=12345;
+   	   long w= 922337203;
+	   float r= 22.44f;
+	   double e=23.2445435;
+	   char k='d';
+	   System.out.println(x);
+	   System.out.println(y);
+	   System.out.println(w);
+           System.out.println(r);
+	   System.out.println(e);
+	   System.out.println(k);
+	   System.out.println("mmmmmm33333mmm");
+	}
+}
+
+class add
+{
+     public static void main(String argsg[])
+	{
+		ading vv=new ading();
+		vv.formateing();
+	}	
+} 	
