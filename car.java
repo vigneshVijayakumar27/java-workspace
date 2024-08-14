65cbd6dd46b48a69dda4831ff139fc43a3962a75
VIGNESH.V <vicky.v20040327@gmail.com>
2024-08-14 11:52:06 +0530

    Car Saling

---
+++ car.java
@@ -0,0 +1,21 @@
+class product
+{
+   public void TATA(String carname,int price)
+     {
+	System.out.println("Carname="+carname);
+	System.out.println("Price="+price);
+     }
+   public void Toyota(String values,)
+     {
+	System.out.println();
+     }
+}
+
+class car
+{
+   public static void main(String arg[])
+     {
+	product vv=new product();
+	vv.display("tata",20000);
+     }
+}
