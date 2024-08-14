2a0865b51a477307cfa78d7b7e59f8b32cc2f0ca
VIGNESH.V <vicky.v20040327@gmail.com>
2024-08-14 11:47:40 +0530

    Add files via upload

---
+++ A_rgu.java
@@ -0,0 +1,18 @@
+class argumentla
+{
+   public void Math()
+    {
+	System.out.println("Hello world");
+	int x=10;
+	int y=10;
+    }
+}
+
+class A_rgu
+{
+   public static void main(String[] args) 
+    {
+	argumentla vv=new argumentla();
+	vv.Math();
+    }
+}
