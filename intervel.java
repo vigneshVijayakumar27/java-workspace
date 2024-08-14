fb9b728ca2252fbac5351965f57187931f42383b
VIGNESH.V <vicky.v20040327@gmail.com>
2024-08-14 11:45:39 +0530

    Add files via upload

---
+++ intervel.java
@@ -0,0 +1,22 @@
+class intervel
+{
+   public static void main(String arg[])
+    {
+	int no=5;
+	while(no<15)
+	{
+		System.out.println(no);
+		no+=1;
+	}
+	int No=10;
+	while(No>0)
+	{
+		System.out.println(No);
+		No-=1;
+	}
+	if(no>2)
+	{
+		System.out.println("hi da");
+	}
+    }
+}
