43ea09fb92fef802f410a8deb5669b6bdfdb92a5
VIGNESH.V <vicky.v20040327@gmail.com>
2024-08-20 09:52:12 +0530

    Do While Loop's

---
+++ Dowhile.java
@@ -0,0 +1,22 @@
+class ading
+{
+   public void formateing()
+	{
+		int x=1;
+		do
+		{
+		    System.out.println("hi "+x);
+		    x+=1;   //x=x+1;
+		}while(x<=10);
+		
+	}
+}
+
+class Dowhile
+{
+     public static void main(String argsg[])
+	{
+		ading vv=new ading();
+		vv.formateing();
+	}	
+} 	
