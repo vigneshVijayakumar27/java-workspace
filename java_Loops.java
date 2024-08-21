0563812c12b9b5a33b51f40526c2e7ea0109d88d
VIGNESH.V <vicky.v20040327@gmail.com>
2024-08-21 10:51:55 +0530

    Java Programming Loops programs.

---
+++ java_Loops.java
@@ -0,0 +1,39 @@
+class cluster
+{
+   public static void main(String arg[])
+    {
+	for(int x=0;x<5;x++)       // 0 1 2 3 4 5
+	{
+		for(int y=0;y<2;y++)       // 0 1
+		{
+			System.out.println("Hi = ");	
+		}
+			System.out.println(" ");
+		for(long i=0;i<3;i++)
+		{
+			System.out.println(i);
+		}
+			
+			System.out.println(" ");
+	}
+
+  // This is a Do-While Statement in java.
+
+	int x=5;
+	do
+	{
+		System.out.println(x);
+		x++;
+	}
+	while(x<=10);
+	System.out.println(" ");
+
+  // This is a While Statement in java
+
+	int y=2;
+	while(y>10)
+	{
+		System.out.println(y);
+	}
+    }
+}
