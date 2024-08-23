3bcfd637def3ca9d8b5689e69f02c064cf76a6d6
VIGNESH.V <vicky.v20040327@gmail.com>
2024-08-23 11:32:59 +0530

    Using Scanner in java.

--- linear.java
+++ linear.java
@@ -1,20 +1,23 @@
 import java.util.Scanner;
 class linear
 {
-   public void tab(int val)
+   public void tab(int table)
     {
-	for(int x=1;x<=15;x++)
+	for(int x=1;x<15;x++)
 	{
-		System.out.println(x+"*"+val+"="+x*val);
+		System.out.println(x+"*"+table+"="+x*table);
 	}
     }
 
    public static void main(String agr[])
     {
 	Scanner total=new Scanner(System.in);
-	int val=total.nextInt();
-	
+	System.out.print("Enter your name = ");
+	String val=total.nextLine();
+	System.out.println("Welcome "+val);
+
 	linear vv=new linear();
-	vv.tab(val);
+	vv.tab(2);
+
     }
 }
