e09230ac5713c7748eb0820e95cbc8e8d004e2b9
VIGNESH.V <vicky.v20040327@gmail.com>
2024-09-04 12:10:28 +0530

    7 States of SquareFeet calculate in java

--- Feet.java
+++ Feet.java
@@ -2,11 +2,12 @@ import java.io.*;
 
 class Feet
 {
-   public static void main(String arg[])
-    {
 	DataInputStream vv=new DataInputStream(System.in);
+   public void calcu(String Land)
+    {
+     if(Land=="Salem")
+     {
 	try{
-		String pro="Land";
 		int rate=2700;
 
 		System.out.print("Enter Your Name :");
@@ -26,10 +27,300 @@ class Feet
 		System.out.println("Address :"+add);
 		System.out.println("PhoneNo. :="+Pho);
 
-		System.out.println(pro);
+		System.out.println(Land);
 		System.out.println("Your SquareFeet : "+feet);
 		System.out.println("Your Land Rate = "+total);
 		System.out.println("-----------------");
 	   }catch(IOException e){}
+      }
+      else if(Land=="Chennai")
+      {
+	try{
+		int rate=5058;
+
+		System.out.print("Enter Your Name :");
+		String name=vv.readLine();
+		System.out.print("Enter Your Address :");
+		String add=vv.readLine();
+		System.out.print("Enter Your PhoneNo. :");
+		int Pho=Integer.valueOf(vv.readLine()).intValue();
+		
+		System.out.print("Enter your SquareFeet  = ");
+		int feet=Integer.valueOf(vv.readLine()).intValue();
+	
+		int total=feet*rate;
+
+		System.out.println("-----------------");
+		System.out.println("Name : "+name);
+		System.out.println("Address :"+add);
+		System.out.println("PhoneNo. :="+Pho);
+
+		System.out.println(Land);
+		System.out.println("Your SquareFeet : "+feet);
+		System.out.println("Your Land Rate = "+total);
+		System.out.println("-----------------");
+	   }catch(IOException e){}
+      }
+      else if(Land=="Madurai")
+      {
+	try{
+		int rate=5698;
+
+		System.out.print("Enter Your Name :");
+		String name=vv.readLine();
+		System.out.print("Enter Your Address :");
+		String add=vv.readLine();
+		System.out.print("Enter Your PhoneNo. :");
+		int Pho=Integer.valueOf(vv.readLine()).intValue();
+		
+		System.out.print("Enter your SquareFeet  = ");
+		int feet=Integer.valueOf(vv.readLine()).intValue();
+	
+		int total=feet*rate;
+
+		System.out.println("-----------------");
+		System.out.println("Name : "+name);
+		System.out.println("Address :"+add);
+		System.out.println("PhoneNo. :="+Pho);
+
+		System.out.println(Land);
+		System.out.println("Your SquareFeet : "+feet);
+		System.out.println("Your Land Rate = "+total);
+		System.out.println("-----------------");
+	   }catch(IOException e){}
+      }
+      else if(Land=="Erode")
+      {
+	try{
+		int rate=3624;
+
+		System.out.print("Enter Your Name :");
+		String name=vv.readLine();
+		System.out.print("Enter Your Address :");
+		String add=vv.readLine();
+		System.out.print("Enter Your PhoneNo. :");
+		int Pho=Integer.valueOf(vv.readLine()).intValue();
+		
+		System.out.print("Enter your SquareFeet  = ");
+		int feet=Integer.valueOf(vv.readLine()).intValue();
+	
+		int total=feet*rate;
+
+		System.out.println("-----------------");
+		System.out.println("Name : "+name);
+		System.out.println("Address :"+add);
+		System.out.println("PhoneNo. :="+Pho);
+
+		System.out.println(Land);
+		System.out.println("Your SquareFeet : "+feet);
+		System.out.println("Your Land Rate = "+total);
+		System.out.println("-----------------");
+	   }catch(IOException e){}
+      }
+      else if(Land=="Coimbatore")
+      {
+	try{
+		int rate=1000;
+
+		System.out.print("Enter Your Name :");
+		String name=vv.readLine();
+		System.out.print("Enter Your Address :");
+		String add=vv.readLine();
+		System.out.print("Enter Your PhoneNo. :");
+		int Pho=Integer.valueOf(vv.readLine()).intValue();
+		
+		System.out.print("Enter your SquareFeet  = ");
+		int feet=Integer.valueOf(vv.readLine()).intValue();
+	
+		int total=feet*rate;
+
+		System.out.println("-----------------");
+		System.out.println("Name : "+name);
+		System.out.println("Address :"+add);
+		System.out.println("PhoneNo. :="+Pho);
+
+		System.out.println(Land);
+		System.out.println("Your SquareFeet : "+feet);
+		System.out.println("Your Land Rate = "+total);
+		System.out.println("-----------------");
+	   }catch(IOException e){}
+      }
+      else if(Land=="Krishnagiri")
+      {
+	try{
+		int rate=1440;
+
+		System.out.print("Enter Your Name :");
+		String name=vv.readLine();
+		System.out.print("Enter Your Address :");
+		String add=vv.readLine();
+		System.out.print("Enter Your PhoneNo. :");
+		int Pho=Integer.valueOf(vv.readLine()).intValue();
+		
+		System.out.print("Enter your SquareFeet  = ");
+		int feet=Integer.valueOf(vv.readLine()).intValue();
+	
+		int total=feet*rate;
+
+		System.out.println("-----------------");
+		System.out.println("Name : "+name);
+		System.out.println("Address :"+add);
+		System.out.println("PhoneNo. :="+Pho);
+
+		System.out.println(Land);
+		System.out.println("Your SquareFeet : "+feet);
+		System.out.println("Your Land Rate = "+total);
+		System.out.println("-----------------");
+	   }catch(IOException e){}
+      }
+      else if(Land=="Dharmapuri")
+      {
+	try{
+		int rate=7748;
+
+		System.out.print("Enter Your Name :");
+		String name=vv.readLine();
+		System.out.print("Enter Your Address :");
+		String add=vv.readLine();
+		System.out.print("Enter Your PhoneNo. :");
+		int Pho=Integer.valueOf(vv.readLine()).intValue();
+		
+		System.out.print("Enter your SquareFeet  = ");
+		int feet=Integer.valueOf(vv.readLine()).intValue();
+	
+		int total=feet*rate;
+
+		System.out.println("-----------------");
+		System.out.println("Name : "+name);
+		System.out.println("Address :"+add);
+		System.out.println("PhoneNo. :="+Pho);
+
+		System.out.println(Land);
+		System.out.println("Your SquareFeet : "+feet);
+		System.out.println("Your Land Rate = "+total);
+		System.out.println("-----------------");
+	   }catch(IOException e){}
+      }
+      else if(Land=="Karur")
+      {
+	try{
+		int rate=3448;
+
+		System.out.print("Enter Your Name :");
+		String name=vv.readLine();
+		System.out.print("Enter Your Address :");
+		String add=vv.readLine();
+		System.out.print("Enter Your PhoneNo. :");
+		int Pho=Integer.valueOf(vv.readLine()).intValue();
+		
+		System.out.print("Enter your SquareFeet  = ");
+		int feet=Integer.valueOf(vv.readLine()).intValue();
+	
+		int total=feet*rate;
+
+		System.out.println("-----------------");
+		System.out.println("Name : "+name);
+		System.out.println("Address :"+add);
+		System.out.println("PhoneNo. :="+Pho);
+
+		System.out.println(Land);
+		System.out.println("Your SquareFeet : "+feet);
+		System.out.println("Your Land Rate = "+total);
+		System.out.println("-----------------");
+	   }catch(IOException e){}
+      }
+      else if(Land=="Nagapattinam")
+      {
+	try{
+		int rate=6667;
+
+		System.out.print("Enter Your Name :");
+		String name=vv.readLine();
+		System.out.print("Enter Your Address :");
+		String add=vv.readLine();
+		System.out.print("Enter Your PhoneNo. :");
+		int Pho=Integer.valueOf(vv.readLine()).intValue();
+		
+		System.out.print("Enter your SquareFeet  = ");
+		int feet=Integer.valueOf(vv.readLine()).intValue();
+	
+		int total=feet*rate;
+
+		System.out.println("-----------------");
+		System.out.println("Name : "+name);
+		System.out.println("Address :"+add);
+		System.out.println("PhoneNo. :="+Pho);
+
+		System.out.println(Land);
+		System.out.println("Your SquareFeet : "+feet);
+		System.out.println("Your Land Rate = "+total);
+		System.out.println("-----------------");
+	   }catch(IOException e){}
+      }
+      else if(Land=="Kanniyakumari")
+       {
+	try{
+		int rate=3400;
+
+		System.out.print("Enter Your Name :");
+		String name=vv.readLine();
+		System.out.print("Enter Your Address :");
+		String add=vv.readLine();
+		System.out.print("Enter Your PhoneNo. :");
+		int Pho=Integer.valueOf(vv.readLine()).intValue();
+		
+		System.out.print("Enter your SquareFeet  = ");
+		int feet=Integer.valueOf(vv.readLine()).intValue();
+	
+		int total=feet*rate;
+
+		System.out.println("-----------------");
+		System.out.println("Name : "+name);
+		System.out.println("Address :"+add);
+		System.out.println("PhoneNo. :="+Pho);
+
+		System.out.println(Land);
+		System.out.println("Your SquareFeet : "+feet);
+		System.out.println("Your Land Rate = "+total);
+		System.out.println("-----------------");
+	   }catch(IOException e){}
+       }
+	else if(Land=="Namakkal")
+       {
+	try{
+		int rate=3703;
+
+		System.out.print("Enter Your Name :");
+		String name=vv.readLine();
+		System.out.print("Enter Your Address :");
+		String add=vv.readLine();
+		System.out.print("Enter Your PhoneNo. :");
+		int Pho=Integer.valueOf(vv.readLine()).intValue();
+		
+		System.out.print("Enter your SquareFeet  = ");
+		int feet=Integer.valueOf(vv.readLine()).intValue();
+	
+		int total=feet*rate;
+
+		System.out.println("-----------------");
+		System.out.println("Name : "+name);
+		System.out.println("Address :"+add);
+		System.out.println("PhoneNo. :="+Pho);
+
+		System.out.println(Land);
+		System.out.println("Your SquareFeet : "+feet);
+		System.out.println("Your Land Rate = "+total);
+		System.out.println("-----------------");
+	   }catch(IOException e){}
+	   
+       	}
+}
+
+   public static void main(String arg[])
+    {
+	System.out.println("Salem,Chennai,Madurai,Erode,Coimbatore,Krishnagiri,Namakkal");
+	System.out.println("-----------------------------------------------------------");
+  	Feet obj=new Feet();
+	obj.calcu("Krishnagiri");
     }
 }
