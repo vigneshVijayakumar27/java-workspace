1da9a1ef7e372590a7101cce72e39edaab2f10e5
VIGNESH.V <vicky.v20040327@gmail.com>
2024-09-11 11:22:32 +0530

    Add files via upload

---
+++ ultra.java
@@ -0,0 +1,21 @@
+class Keywords
+{
+  public void Thore()
+   {
+	" "     -> It is a String keyword,Using many words with in the single line.
+	""" """ -> It is a String keyword,Using many words with one or more lines.
+	\t      -> It is a move five spaces.
+	//      -> It is a single line comment keyword.
+	/* */   -> it is a one or more line comment keyword.
+	
+   }  
+}
+
+class ultra
+{
+  public static void main(String arg[])
+    {
+	Keywords vv=new Keywords();
+	vv.Thore();
+    }
+}
