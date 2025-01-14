842767cc8fef183e98a74d08eb92abda21d08f78
VIGNESH.V <vicky.v20040327@gmail.com>
2025-01-14 11:38:47 +0530

    for loop and current time in java

---
+++ TT.java
@@ -0,0 +1,17 @@
+import java.util.Date;
+
+class TT
+{
+   public static void main(String arg[])
+    {
+	for(int i=0;i<=4;i++)
+	for(int j=1;j<=4;j++)
+	{
+	System.out.print(j);
+	}
+	System.out.println(" ");
+	Date d1=new Date();
+	System.out.println(d1);
+	
+    }
+}
