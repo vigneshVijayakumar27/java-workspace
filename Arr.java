94fa9a63e0d5f7465fe65fd1bdd6db8691fdfbb9
VIGNESH.V <vicky.v20040327@gmail.com>
2025-01-23 21:06:06 +0530

    Array simple code

---
+++ Arr.java
@@ -0,0 +1,13 @@
+class Arr
+{
+	public static void main(String arg[])
+	{
+		String name[]= new String[2];
+		name[0]="Maran";
+		name[1]="Villu";
+		for(int i=0;i<name.length;i++)
+		{
+			System.out.println("Your name : "+name[i]);
+		}
+	}
+}
