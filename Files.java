76acbb8840ab6a59c6be6cd30c1028b7529a8272
VIGNESH.V <vicky.v20040327@gmail.com>
2025-02-13 19:40:56 +0530

    Using IOStream in File read and write in java

---
+++ Files.java
@@ -0,0 +1,33 @@
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.util.*;
+class Files
+{
+	static Scanner ip=new Scanner(System.in);
+	public static void main(String arg[])throws Exception
+	{
+		FileOutputStream fo=new FileOutputStream("S:/Captures/mydata.txt");
+		
+		String word="Enter your Name : ";
+		
+		byte a[]=word.getBytes();
+		
+		fo.write(a);
+		
+		FileInputStream fi=new FileInputStream("S:/Captures/mydata.txt");
+		
+		int i=0;
+		while((i=fi.read())!=-1)
+		{
+			System.out.print((char)i);
+		} 
+		
+		FileOutputStream FO=new FileOutputStream("mydata.txt");
+		
+		String word2=ip.nextLine();
+		
+		byte b[]=word2.getBytes();
+		
+		FO.write(b);
+	}
+}
